2fafe46e3f92faf912997e042f0183a176687f93
Nextor Leon <nextor@gmail.com>
2018-11-30 09:49:41 -0800

    FOO

--- normalizer.py
+++ normalizer.py
@@ -3,32 +3,14 @@
 import random
 
 
-def get_dictionary( is_to_compress ):
 
-    to_compress = []
-    to_decompress = []
-
-    for i in range(0, 256):
-        to_compress.append([])
-        to_decompress.append([0]*256)
-
-    for i in range(0, 256):
-        for j in range(0, 256):
-            if (j-i) < 0:
-                to_compress[i].append(256+(j-i))
-                to_decompress[i][256+(j-i)] = j
-            else:
-                to_compress[i].append(j-i)
-                to_decompress[i][(j-i)] = j
-
-    return to_compress if is_to_compress else to_decompress
 
 class Compress:
     """format a random byte string to compress or decompress"""
     def __init__(self, is_to_compress):
-        self.dictionary = get_dictionary(is_to_compress)
-        self.previous = 0
         self.is_to_compress = is_to_compress
+        self.previous = 0
+        self.dictionary = self.get_dictionary()
 
     def process(self, byte):
         """Prepare a byte to be compressed or decompressed"""
@@ -41,6 +23,24 @@ class Compress:
 
         return to_write
 
+    def get_dictionary(self):
+        to_compress = []
+        to_decompress = []
+
+        for i in range(0, 256):
+            to_compress.append([])
+            to_decompress.append([0]*256)
+
+        for i in range(0, 256):
+            for j in range(0, 256):
+                if (j-i) < 0:
+                    to_compress[i].append(256+(j-i))
+                    to_decompress[i][256+(j-i)] = j
+                else:
+                    to_compress[i].append(j-i)
+                    to_decompress[i][(j-i)] = j
+
+        return to_compress if self.is_to_compress else to_decompress
 
 
 def test_transform():
